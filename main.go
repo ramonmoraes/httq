d655b870aa1bba03f4bdf8fd6cb04b8106fe77be
Ramon Moraes - Dart <ramon.moraes94@gmail.com>
2020-09-03 17:08:49 -0300

    main func using httq as a server

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"ramonmoraes/httq/infra"
+
+	"github.com/gorilla/mux"
+)
+
+type HTTQ interface {
+	GetMessage(w http.ResponseWriter, r *http.Request)
+	WriteMessage(w http.ResponseWriter, r *http.Request)
+	GetPrefix() string
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	router := mux.NewRouter()
+	infra := &infra.KafkaHTTQ{}
+
+	path := fmt.Sprintf("/%s/{key}", infra.GetPrefix())
+	router.HandleFunc(path, infra.GetMessage).Methods("GET")
+	router.HandleFunc(path, infra.WriteMessage).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
